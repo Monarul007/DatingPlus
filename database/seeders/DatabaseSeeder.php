d076fd91da484697f4802565d5e63fa031d03f3b
monarul007 <monarul007@gmail.com>
2022-04-04 11:22:27 +0600

    DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,9 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,11 +19,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => 'Monarul Islam',
+            'email' => 'monarul007@gmail.com',
+            'password' => Hash::make('monarul007'),
+        ]);
         \App\Models\User::factory(50)->create();
         $role = Role::create(['name' => 'super-admin']);
         $role->givePermissionTo(Permission::all());
 
-        $user = User::find(11);
+        $user = User::find(1);
         $user->assignRole('super-admin');
     }
 }
